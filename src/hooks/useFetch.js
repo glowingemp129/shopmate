d1da3566989b2d1ccbd1c2da6d383f48c663a254
Taqi Ul Hassan <glowingemp129@glowingsoft.com>
2025-09-29 14:08:30 +0500

    create custom hook for fetching Data

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react";
+
+ export const useFetch = (url) => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url);
+            const data = await response.json();
+            setData(data);
+        }
+        fetchData();
+    }, [url]);
+
+    return {data};
+}
